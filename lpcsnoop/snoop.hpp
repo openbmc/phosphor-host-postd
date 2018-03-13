efb80da071fb64dd3a12b314c5f5a8b6dd0e99d5
Patrick Venture <venture@google.com>
2018-03-12 18:56:14 -0700

    Initial check-in of LPC Snoop Broadcast Daemon
    This is a simple daemon which reads a file interface
    from an lpc-snoop driver and broadcasts the values read.
    
    It presently assumes there's /dev/aspeed-lpc-snoop0, however
    this could be made a command line parameter.
    
    Change-Id: Ic8e7511de619d93bf1cffd9a096c92315f870946
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ lpcsnoop/snoop.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+/* The LPC snoop on port 80h is mapped to this dbus path. */
+#define SNOOP_OBJECTPATH "/xyz/openbmc_project/state/boot/raw"
+/* The LPC snoop on port 80h is mapped to this dbus service. */
+#define SNOOP_BUSNAME "xyz.openbmc_project.State.Boot.Raw"
+
