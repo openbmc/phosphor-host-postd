9763284ecee4f56f3c271e0ee41af316472d9e49
Jayanth Othayoth <ojayanth@gmail.com>
2024-12-17 06:28:10 -0600

    clang-tidy: Replace NULL with nullptr
    Replaced all instances of NULL with nullptr to improve type safety
    and clarity, as nullptr is the modern C++ standard for null pointers.
    
    Tested: Build verified
    
    Change-Id: I81f6b220f0f01ce1cdc631aeb333c0a26edf9f58
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

--- main.cpp
+++ main.cpp
@@ -274,12 +274,12 @@ int main(int argc, char* argv[])
 #ifdef ENABLE_IPMI_SNOOP
         {"host", optional_argument, NULL, 'h'},
 #else
-        {"device", optional_argument, NULL, 'd'},
-        {"rate-limit", optional_argument, NULL, 'r'},
-        {"bytes",  required_argument, NULL, 'b'},
+        {"device", optional_argument, nullptr, 'd'},
+        {"rate-limit", optional_argument, nullptr, 'r'},
+        {"bytes",  required_argument, nullptr, 'b'},
 #endif
-        {"verbose", no_argument, NULL, 'v'},
-        {0, 0, 0, 0}
+        {"verbose", no_argument, nullptr, 'v'},
+        {nullptr, 0, nullptr, 0}
     };
     // clang-format on
 
@@ -291,7 +291,8 @@ int main(int argc, char* argv[])
 #endif
         "v";
 
-    while ((opt = getopt_long(argc, argv, optstring, long_options, NULL)) != -1)
+    while ((opt = getopt_long(argc, argv, optstring, long_options, nullptr)) !=
+           -1)
     {
         switch (opt)
         {
